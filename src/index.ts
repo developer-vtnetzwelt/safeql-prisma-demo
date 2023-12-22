0b023c6eb09ea921ba1fced7fbe0df3465a8811f
saurabh.jindal <saurabh.jindal@vtnetzwelt.com>
2023-12-22 15:31:09 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { PrismaClient } from "@prisma/client";
 async function main() {
   const prisma = new PrismaClient();
 
-  const [r2] = await prisma.$queryRaw<{ id: number }[]>`SELECT iddss FROM "User" LIMIT 1`;
+  const [r2] = await prisma.$queryRaw<{ id: number }[]>`SELECT id
+   FROM "User" LIMIT 1`;
   const r1 = await prisma.user.findFirst({ select: { id: true } });
 
   console.log({ r1, r2 });
